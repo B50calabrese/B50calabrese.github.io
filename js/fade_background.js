ff39488f1b76bd7aec83b4c7c212044896509998
B50calabrese <antoniobrese@aol.com>
2015-05-13 16:48:53 -0400

    More fixing of stuff to perfect it

--- js/fade_background.js
+++ js/fade_background.js
@@ -1,6 +1,7 @@
-var FINAL_BACKGROUND_COLOR = 205;
+var FINAL_BACKGROUND_COLOR = 235;
 var START_BACKGROUND_COLOR = 255;
-var INTERVAL = 10;
+var INTERVAL = 5;
+var TIMEOUT = 100;
 var current_background = 255;
 var fade_in_call;
 var fade_out_call;
@@ -15,7 +16,7 @@ function fade_in(div1) {
 	div1.style.background = '#' + str_background + str_background + str_background;
 	if (current_background != FINAL_BACKGROUND_COLOR) {
 		current_background = current_background - INTERVAL;
-		fade_in_call = setTimeout(function() { fade_in(div1); }, 250);
+		fade_in_call = setTimeout(function() { fade_in(div1); }, TIMEOUT);
 	}
 }
 
@@ -29,6 +30,6 @@ function fade_out(div1) {
 	div1.style.background = '#' + str_background + str_background + str_background;
 	if (current_background != START_BACKGROUND_COLOR) {
 		current_background = current_background + INTERVAL;
-		fade_out_call = setTimeout(function() { fade_out(div1); }, 250);
+		fade_out_call = setTimeout(function() { fade_out(div1); }, TIMEOUT);
 	}	
 }
