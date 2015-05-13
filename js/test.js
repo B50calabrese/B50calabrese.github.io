336863d1d43cc7073a5c482506ff3e840bf10222
B50calabrese <antoniobrese@aol.com>
2015-05-13 14:34:40 -0400

    Still trying to fix the issue

--- js/test.js
+++ js/test.js
@@ -1,11 +1,3 @@
-document.getElementById('testId').onmouseover=function(){
-    underline_function(document.getElementById('testId'));
-};
-
-document.getElementById('testId').onmouseout=function(){
-    non_underline_function(document.getElementById('testId'));
-};
-
 function underline_function(div1) {
     div1.style.background = '#f00';
 }
