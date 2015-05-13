f5100875150c7c9b8f20a35a74fc2da7bd79514f
B50calabrese <antoniobrese@aol.com>
2015-05-13 16:41:28 -0400

    Trying to fix issue with the timeout being called too often.

--- js/fade_background.js
+++ js/fade_background.js
@@ -1,26 +1,34 @@
 var FINAL_BACKGROUND_COLOR = 'd0d0d0';
 var current_background = 15;
+var fade_in_call;
+var fade_out_call;
 
 // We will call this on mouse hover. It will fade the background in over 1.5 sec
 function fade_in(div1) {
+	// Clear the fade out call
+	clearTimeout(fade_out_call);
+
 	// The hexadecimal string representation of the number
 	var str_background = current_background.toString(16);
 	div1.style.background = '#' + str_background + '0' + str_background + '0'
 			+ str_background + '0';
 	if (current_background != 13) {
 		current_background = current_background - 1;
-		setTimeout(function() { fade_in(div1); }, 500);
+		fade_in_call = setTimeout(function() { fade_in(div1); }, 500);
 	}
 }
 
 // We will call this on mouse out. It will fade the background back over 1.5 sec
 function fade_out(div1) {
+	// Clear the fade in call
+	clearTimeout(fade_in_call);
+
 	// The hexadecimal string representation of the number
 	var str_background = current_background.toString(16);
 	div1.style.background = '#' + str_background + '0' + str_background + '0'
 			+ str_background + '0';
 	if (current_background != 15) {
 		current_background = current_background + 1;
-		setTimeout(function() { fade_out(div1); }, 500);
+		fade_out_call = setTimeout(function() { fade_out(div1); }, 500);
 	}	
 }
