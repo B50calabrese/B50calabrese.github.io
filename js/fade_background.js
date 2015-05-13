71dd306c106bc5d7203963f407e3ab2953bd9af7
B50calabrese <antoniobrese@aol.com>
2015-05-13 16:42:50 -0400

    Trying to get it to look better

--- js/fade_background.js
+++ js/fade_background.js
@@ -1,9 +1,9 @@
 var FINAL_BACKGROUND_COLOR = 'd0d0d0';
-var current_background = 15;
+var current_background = 16;
 var fade_in_call;
 var fade_out_call;
 
-// We will call this on mouse hover. It will fade the background in over 1.5 sec
+// We will call this on mouse hover. It will fade the background in
 function fade_in(div1) {
 	// Clear the fade out call
 	clearTimeout(fade_out_call);
@@ -14,11 +14,11 @@ function fade_in(div1) {
 			+ str_background + '0';
 	if (current_background != 13) {
 		current_background = current_background - 1;
-		fade_in_call = setTimeout(function() { fade_in(div1); }, 500);
+		fade_in_call = setTimeout(function() { fade_in(div1); }, 300);
 	}
 }
 
-// We will call this on mouse out. It will fade the background back over 1.5 sec
+// We will call this on mouse out. It will fade the background back out
 function fade_out(div1) {
 	// Clear the fade in call
 	clearTimeout(fade_in_call);
@@ -27,8 +27,8 @@ function fade_out(div1) {
 	var str_background = current_background.toString(16);
 	div1.style.background = '#' + str_background + '0' + str_background + '0'
 			+ str_background + '0';
-	if (current_background != 15) {
+	if (current_background != 16) {
 		current_background = current_background + 1;
-		fade_out_call = setTimeout(function() { fade_out(div1); }, 500);
+		fade_out_call = setTimeout(function() { fade_out(div1); }, 300);
 	}	
 }
